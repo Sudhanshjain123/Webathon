1c7ecb7a5a40c8e65078b6a2ba84164b9de6ba26
Abhibhavya Gupta <abhibhavya.gupta2005@gmail.com>
2026-02-20 15:36:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var water;
+function Water(){
+    const inputelem=document.getElementById('water');
+     water=inputelem.value;
+
+}
+console.log(water);
+
+function Steps(){
+    const inputelem=document.getElementById('water-intake');
+    let water=inputelem.value;
+    console.log(water);
+}
+function Sleep(){
+    const inputelem=document.getElementById('water-intake');
+    let water=inputelem.value;
+    console.log(water);
+}
+function Mood(){
+    const inputelem=document.getElementById('water-intake');
+    let water=inputelem.value;
+    console.log(water);
+}
